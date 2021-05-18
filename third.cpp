cdfffd993215f042df08013728e4b9059dcd2e89
Devansh Kumar Jha <77242838+dkjha20@users.noreply.github.com>
2021-05-18 18:37:44 +0530

    Update third.cpp

--- third.cpp
+++ third.cpp
@@ -15,12 +15,12 @@ void dkj()            // This function is just for fast input-output operations
          
 int main()
 {
-    vector <char> a;
-    char sign[2];
-    int ans,num[3],i=0,j=0,sup_flag=1;
-    char ch[100];   gets(ch);
+    vector <char> a;    // vector for storage of sub-parts of input
+    char sign[2];       // array to store algebraic operations
+    int ans,num[3],i=0,j=0,sup_flag=1;   // variables of use
+    char ch[100];   gets(ch);            // array to store characters
 
-    for(int t=0;t<strlen(ch);t++) {
+    for(int t=0;t<strlen(ch);t++) {                // loop to get the numbers out of the expression in integer format
        if(ch[t]=='+' || ch[t]=='-' || ch[t]=='*' || ch[t]=='/' || t==strlen(ch)-1) {
               if(t==strlen(ch)-1) { a.push_back(ch[t]); }
               else { sign[i]=ch[t];  i++; } 
@@ -32,15 +32,57 @@ int main()
               }
               
               num[j]=p; 
-              if(!(num[j]>=0 && num[j]<=1000)) sup_flag=0;
+              if(!(num[j]>=0 && num[j]<=1000)) sup_flag=0;   // check for numbers being in correct range
               j++;
               a.clear();
        }
       else {  a.push_back(ch[t]);  }
     }
 
-    if(sup_flag!=0)  { cout<<(num[0]+num[1]+num[2]); }
-    else cout<<-1;
+    try {                                      // calculation of expression along with expression handling
+       if(sup_flag!=0) {
+           if(sign[0]=='*') {
+                 if(sign[1]=='+') { cout<<(num[0]*num[1]+num[2]); }
+                 else if(sign[1]=='-') { cout<<(num[0]*num[1]-num[2]); }
+                 else if(sign[1]=='*') { cout<<(num[0]*num[1]*num[2]); }
+                 else { 
+                     if(num[2]==0) throw 1;
+                     cout<<(num[0]*num[1]/num[2]);  } 
+            }
+            else if(sign[0]=='/') {
+                 if(sign[1]=='+') { 
+                     if(num[1]==0) throw 1;
+                     cout<<(num[0]/num[1]+num[2]);  }
+                 else if(sign[1]=='-') { 
+                     if(num[1]==0) throw 1;
+                     cout<<(num[0]/num[1]-num[2]); }
+                 else if(sign[1]=='*') { 
+                     if(num[1]==0) throw 1;
+                     cout<<(num[0]/num[1]*num[2]); }
+                 else { 
+                     if(num[1]==0 || num[2]==0) throw 1;
+                     cout<<(num[0]/num[1]/num[2]); } 
+            }
+            else if(sign[0]=='+') {
+                 if(sign[1]=='+') { cout<<(num[0]+num[1]+num[2]); }
+                 else if(sign[1]=='-') { cout<<(num[0]+num[1]-num[2]); }
+                 else if(sign[1]=='*') { cout<<(num[0]+num[1]*num[2]); }
+                 else { 
+                     if(num[2]==0) throw 1;
+                     cout<<(num[0]+num[1]/num[2]); } 
+            }
+            else {
+                 if(sign[1]=='+') { cout<<(num[0]-num[1]+num[2]); }
+                 else if(sign[1]=='-') { cout<<(num[0]-num[1]-num[2]); }
+                 else if(sign[1]=='*') { cout<<(num[0]-num[1]*num[2]); }
+                 else { 
+                     if(num[2]==0) throw 1;
+                     cout<<(num[0]-num[1]/num[2]); } 
+            }
+       }     
+       else cout<<-1;               // in case of overflow out of range
+    } 
+    catch(int x) { cout<<"Exception"; }  // division by zero error
 
     return 0;
 }
